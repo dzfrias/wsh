9151b64c3b406d4b99d8957a68e1d8f134ddedec
Diego <96022404+dzfrias@users.noreply.github.com>
2024-02-03 19:13:12 -0800

    Change Value representation

--- crates/wsh-lang/src/interpreter/shell_v2/mod.rs
+++ crates/wsh-lang/src/interpreter/shell_v2/mod.rs
@@ -15,7 +15,7 @@ use crate::{
     shell_v2::{
         builtins::Builtin,
         error::{Error, ErrorKind, WithPosition},
-        value::{Obj, ObjPtr, ValueType},
+        value::ValueType,
     },
     v2::{
         ast::{
@@ -27,7 +27,6 @@ use crate::{
 };
 
 pub struct Shell {
-    to_free: Vec<ObjPtr>,
     current_pos: usize,
     last_status: i32,
 }
@@ -35,7 +34,6 @@ pub struct Shell {
 impl Shell {
     pub fn new() -> Self {
         Shell {
-            to_free: vec![],
             current_pos: 0,
             last_status: 0,
         }
@@ -125,22 +123,17 @@ impl Shell {
     fn eval_expr(&mut self, ast: &Ast, expr: Node) -> Result<Value> {
         self.current_pos = expr.offset();
         let val = match expr.kind() {
-            NodeKind::String(s) => {
-                // Will this result in an invalid ptr deref when ast is dropped?
-                let ptr = Obj::string(s.deref(ast));
-                self.to_free.push(ptr);
-                Value::obj(ptr)
-            }
-            NodeKind::Number(f) => Value::number(f.0),
+            NodeKind::String(s) => Value::String(s.deref(ast).to_string()),
+            NodeKind::Number(f) => Value::Number(f.0),
             NodeKind::Boolean(b) => match b {
-                Boolean::True => Value::TRUE,
-                Boolean::False => Value::FALSE,
+                Boolean::True => Value::Boolean(true),
+                Boolean::False => Value::Boolean(false),
             },
             NodeKind::Ident(_) => todo!(),
             NodeKind::EnvVar(env_var) => self.eval_env_var(ast, *env_var),
             NodeKind::Binop(binop) => self.eval_binop(ast, binop)?,
             NodeKind::Unop(unop) => self.eval_unop(ast, unop)?,
-            NodeKind::LastStatus(_) => Value::number(self.last_status as f64),
+            NodeKind::LastStatus(_) => Value::Number(self.last_status as f64),
             NodeKind::HomeDir(h) => self.eval_home_dir(ast, h)?,
             NodeKind::Capture(_) => todo!(),
             _ => unreachable!("should not be called on non-expr"),
@@ -151,35 +144,21 @@ impl Shell {
     fn eval_binop(&mut self, ast: &Ast, binop: &Binop) -> Result<Value> {
         let lhs = self.eval_expr(ast, binop.lhs.deref(ast))?;
         let rhs = self.eval_expr(ast, binop.rhs.deref(ast))?;
-        // SAFETY: types are being checked before casting
-        let val = unsafe {
-            match (lhs.type_(), rhs.type_()) {
-                (ValueType::Number, ValueType::Number) => {
-                    self.eval_numeric_binop(binop.op, lhs.as_num(), rhs.as_num())
-                }
-                (ValueType::Number, ValueType::Object(_))
-                    if rhs.as_obj().coerce_number().is_some() =>
-                {
-                    self.eval_numeric_binop(
-                        binop.op,
-                        lhs.as_num(),
-                        rhs.as_obj().coerce_number().unwrap(),
-                    )
-                }
-                (ValueType::Object(_), ValueType::Object(_)) => {
-                    self.eval_object_binop(binop.op, lhs.as_obj(), rhs.as_obj())?
-                }
-                (ValueType::Boolean, ValueType::Boolean) => {
-                    self.eval_bool_binop(binop.op, lhs.as_bool(), rhs.as_bool())?
-                }
-                // TODO: string and number infix op
-                _ => {
-                    return Err(self.error(ErrorKind::BinopTypeError {
-                        op: binop.op,
-                        lhs: lhs.type_(),
-                        rhs: rhs.type_(),
-                    }))
-                }
+        let (lhs_ty, rhs_ty) = (lhs.type_(), rhs.type_());
+        let val = match (lhs, rhs) {
+            (Value::Number(a), Value::Number(b)) => self.eval_numeric_binop(binop.op, a, b),
+            (Value::Number(a), Value::String(s)) if s.parse::<f64>().is_ok() => {
+                self.eval_numeric_binop(binop.op, a, s.parse().unwrap())
+            }
+            (Value::String(s1), Value::String(s2)) => self.eval_object_binop(binop.op, s1, s2)?,
+            (Value::Boolean(b1), Value::Boolean(b2)) => self.eval_bool_binop(binop.op, b1, b2)?,
+            // TODO: string and number infix op
+            _ => {
+                return Err(self.error(ErrorKind::BinopTypeError {
+                    op: binop.op,
+                    lhs: lhs_ty,
+                    rhs: rhs_ty,
+                }))
             }
         };
         Ok(val)
@@ -187,20 +166,17 @@ impl Shell {
 
     fn eval_unop(&mut self, ast: &Ast, unop: &Unop) -> Result<Value> {
         let expr = self.eval_expr(ast, unop.expr.deref(ast))?;
-        // SAFETY: types are being checked before casting
-        let val = unsafe {
-            match expr.type_() {
-                ValueType::Number => self.eval_numeric_unop(unop.op, expr.as_num())?,
-                ValueType::Boolean => self.eval_bool_unop(unop.op, expr.as_bool())?,
-                ValueType::Object(_) if expr.as_obj().coerce_number().is_some() => {
-                    self.eval_numeric_unop(unop.op, expr.as_obj().coerce_number().unwrap())?
-                }
-                _ => {
-                    return Err(self.error(ErrorKind::UnopTypeError {
-                        op: unop.op,
-                        type_: expr.type_(),
-                    }))
-                }
+        let val = match expr {
+            Value::Number(n) => self.eval_numeric_unop(unop.op, n)?,
+            Value::Boolean(b) => self.eval_bool_unop(unop.op, b)?,
+            Value::String(s) if s.parse::<f64>().is_ok() => {
+                self.eval_numeric_unop(unop.op, s.parse::<f64>().unwrap())?
+            }
+            _ => {
+                return Err(self.error(ErrorKind::UnopTypeError {
+                    op: unop.op,
+                    type_: expr.type_(),
+                }))
             }
         };
         Ok(val)
@@ -208,8 +184,8 @@ impl Shell {
 
     fn eval_numeric_unop(&mut self, op: UnopKind, f: f64) -> Result<Value> {
         let val = match op {
-            UnopKind::Neg => Value::number(-f),
-            UnopKind::Sign => Value::number(f),
+            UnopKind::Neg => Value::Number(-f),
+            UnopKind::Sign => Value::Number(f),
             UnopKind::Bang => {
                 return Err(self.error(ErrorKind::UnopTypeError {
                     op,
@@ -220,31 +196,28 @@ impl Shell {
         Ok(val)
     }
 
-    fn eval_object_binop(&mut self, op: BinopKind, lhs: ObjPtr, rhs: ObjPtr) -> Result<Value> {
-        let val = match (&*lhs, &*rhs) {
-            (Obj::String(s), Obj::String(u)) => match op {
-                BinopKind::Add => {
-                    let ptr = s.concat(u);
-                    self.to_free.push(ptr);
-                    Value::obj(ptr)
-                }
-                BinopKind::Eq => Value::bool(s == u),
-                BinopKind::Ne => Value::bool(s != u),
-                _ => {
-                    return Err(self.error(ErrorKind::BinopTypeError {
-                        op,
-                        lhs: ValueType::Object(lhs.type_()),
-                        rhs: ValueType::Object(rhs.type_()),
-                    }))
-                }
-            },
+    fn eval_object_binop(&mut self, op: BinopKind, mut lhs: String, rhs: String) -> Result<Value> {
+        let val = match op {
+            BinopKind::Add => {
+                lhs.push_str(&rhs);
+                Value::String(lhs)
+            }
+            BinopKind::Eq => Value::Boolean(lhs == rhs),
+            BinopKind::Ne => Value::Boolean(lhs != rhs),
+            _ => {
+                return Err(self.error(ErrorKind::BinopTypeError {
+                    op,
+                    lhs: ValueType::String,
+                    rhs: ValueType::String,
+                }))
+            }
         };
         Ok(val)
     }
 
     fn eval_bool_unop(&mut self, op: UnopKind, b: bool) -> Result<Value> {
         let val = match op {
-            UnopKind::Neg => Value::bool(!b),
+            UnopKind::Neg => Value::Boolean(!b),
             _ => {
                 return Err(self.error(ErrorKind::UnopTypeError {
                     op,
@@ -257,23 +230,23 @@ impl Shell {
 
     fn eval_numeric_binop(&mut self, op: BinopKind, lhs: f64, rhs: f64) -> Value {
         match op {
-            BinopKind::Add => Value::number(lhs + rhs),
-            BinopKind::Sub => Value::number(lhs - rhs),
-            BinopKind::Div => Value::number(lhs / rhs),
-            BinopKind::Mul => Value::number(lhs * rhs),
-            BinopKind::Lt => Value::bool(lhs < rhs),
-            BinopKind::Gt => Value::bool(lhs > rhs),
-            BinopKind::Le => Value::bool(lhs <= rhs),
-            BinopKind::Ge => Value::bool(lhs >= rhs),
-            BinopKind::Eq => Value::bool(lhs == rhs),
-            BinopKind::Ne => Value::bool(lhs != rhs),
+            BinopKind::Add => Value::Number(lhs + rhs),
+            BinopKind::Sub => Value::Number(lhs - rhs),
+            BinopKind::Div => Value::Number(lhs / rhs),
+            BinopKind::Mul => Value::Number(lhs * rhs),
+            BinopKind::Lt => Value::Boolean(lhs < rhs),
+            BinopKind::Gt => Value::Boolean(lhs > rhs),
+            BinopKind::Le => Value::Boolean(lhs <= rhs),
+            BinopKind::Ge => Value::Boolean(lhs >= rhs),
+            BinopKind::Eq => Value::Boolean(lhs == rhs),
+            BinopKind::Ne => Value::Boolean(lhs != rhs),
         }
     }
 
     fn eval_bool_binop(&mut self, op: BinopKind, lhs: bool, rhs: bool) -> Result<Value> {
         let val = match op {
-            BinopKind::Eq => Value::bool(lhs == rhs),
-            BinopKind::Ne => Value::bool(lhs != rhs),
+            BinopKind::Eq => Value::Boolean(lhs == rhs),
+            BinopKind::Ne => Value::Boolean(lhs != rhs),
             _ => {
                 return Err(self.error(ErrorKind::BinopTypeError {
                     op,
@@ -288,16 +261,12 @@ impl Shell {
     fn eval_home_dir(&mut self, _ast: &Ast, _home_dir: &HomeDir) -> Result<Value> {
         let path = dirs::home_dir()
             .ok_or(ErrorKind::HomeDirNotFound)
-            .with_position(self.current_pos)?
-            .into_boxed_path();
-        // It's okay to leak the box here because we clean it up later
-        let home = Box::leak(path)
+            .with_position(self.current_pos)?;
+        let home = path
             .to_str()
             .ok_or(ErrorKind::HomeDirInvalidUTF8)
             .with_position(self.current_pos)?;
-        let ptr = Obj::string(home);
-        self.to_free.push(ptr);
-        Ok(Value::obj(ptr))
+        Ok(Value::String(home.to_owned()))
     }
 
     fn make_cmd(&mut self, ast: &Ast, cmd: &ast::Command) -> Result<pipeline::Command<Self>> {
@@ -324,12 +293,8 @@ impl Shell {
 
     fn eval_env_var(&mut self, ast: &Ast, env_var: EnvVarHandle) -> Value {
         let env_var = env_var.deref(ast);
-        let value = std::env::var(&**env_var)
-            .unwrap_or_default()
-            .into_boxed_str();
-        let ptr = Obj::string(Box::leak(value));
-        self.to_free.push(ptr);
-        Value::obj(ptr)
+        let value = std::env::var(&**env_var).unwrap_or_default();
+        Value::String(value)
     }
 
     fn error(&self, kind: ErrorKind) -> Error {
@@ -340,13 +305,5 @@ impl Shell {
 impl Default for Shell {
     fn default() -> Self {
         Self::new()
-    }
-}
-
-impl Drop for Shell {
-    fn drop(&mut self) {
-        for val in &self.to_free {
-            unsafe { val.free() }
-        }
     }
 }
--- crates/wsh-lang/src/interpreter/shell_v2/value.rs
+++ crates/wsh-lang/src/interpreter/shell_v2/value.rs
@@ -1,206 +1,50 @@
-use core::fmt;
-use std::{ops::Deref, slice};
+use std::fmt;
 
-#[derive(Clone, Copy)]
-pub struct Value(u64);
+#[derive(Clone)]
+pub enum Value {
+    Number(f64),
+    String(String),
+    Boolean(bool),
+    Null,
+}
 
 impl Value {
-    pub const TRUE: Value = Value(Self::QNAN | Self::TAG_TRUE);
-    pub const FALSE: Value = Value(Self::QNAN | Self::TAG_FALSE);
-    pub const NULL: Value = Value(Self::QNAN | Self::TAG_NULL);
-
-    const QNAN: u64 = 0x7ffc000000000000;
-    const TAG_NULL: u64 = 1;
-    const TAG_FALSE: u64 = 2;
-    const TAG_TRUE: u64 = 3;
-    const SIGN_BIT: u64 = 1 << 63;
-
-    pub fn type_(self) -> ValueType {
-        if (self.0 & Self::QNAN) != Self::QNAN {
-            return ValueType::Number;
-        }
-        if self.0 & Self::SIGN_BIT == Self::SIGN_BIT {
-            let ty = unsafe { self.as_obj() }.type_();
-            return ValueType::Object(ty);
-        }
-        // Tag
-        match self.0 & 0b11 {
-            Self::TAG_FALSE | Self::TAG_TRUE => ValueType::Boolean,
-            Self::TAG_NULL => ValueType::Null,
-            _ => unreachable!(),
-        }
-    }
-
-    pub fn bool(b: bool) -> Self {
-        if b {
-            Self::TRUE
-        } else {
-            Self::FALSE
-        }
-    }
-
-    pub fn number(f: f64) -> Self {
-        Value(f.to_bits())
-    }
-
-    pub fn obj(ptr: ObjPtr) -> Self {
-        Self(Self::SIGN_BIT | Self::QNAN | ptr.0 as u64)
-    }
-
-    pub unsafe fn as_num(self) -> f64 {
-        f64::from_bits(self.0)
-    }
-
-    pub unsafe fn as_bool(self) -> bool {
-        (self.0 & 0b11) == Self::TAG_TRUE
-    }
-
-    pub unsafe fn is_null(self) -> bool {
-        (self.0 & 1) == 1
-    }
-
-    pub unsafe fn as_obj(self) -> ObjPtr {
-        ObjPtr((self.0 & !(Self::SIGN_BIT | Self::QNAN)) as *mut Obj)
-    }
-}
-
-impl fmt::Display for Value {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        // SAFETY: these types can be safely casted
-        unsafe {
-            match self.type_() {
-                ValueType::Number => write!(f, "{}", self.as_num()),
-                ValueType::Null => write!(f, "null"),
-                ValueType::Boolean => write!(f, "{}", self.as_bool()),
-                ValueType::Object(_) => write!(f, "{}", &*self.as_obj()),
-            }
-        }
-    }
-}
-
-#[derive(Clone, Copy)]
-pub struct ObjPtr(*mut Obj);
-
-impl ObjPtr {
-    pub unsafe fn free(self) {
-        let _ = Box::from_raw(self.0);
-    }
-}
-
-impl Deref for ObjPtr {
-    type Target = Obj;
-
-    fn deref(&self) -> &Self::Target {
-        unsafe { &*self.0 as &Self::Target }
-    }
-}
-
-pub enum Obj {
-    String(StringObj),
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum ObjType {
-    String,
-}
-
-impl fmt::Display for Obj {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    pub fn type_(&self) -> ValueType {
         match self {
-            Obj::String(s) => write!(f, "{s}"),
+            Value::Number(_) => ValueType::Number,
+            Value::String(_) => ValueType::String,
+            Value::Boolean(_) => ValueType::Boolean,
+            Value::Null => ValueType::Null,
         }
     }
 }
-
-impl Obj {
-    pub fn coerce_number(&self) -> Option<f64> {
-        match self {
-            Obj::String(s) => s.as_str().parse::<f64>().ok(),
-        }
-    }
-
-    pub fn string(s: &str) -> ObjPtr {
-        let s_obj = StringObj {
-            len: s.len(),
-            chars: s.as_bytes().as_ptr(),
-        };
-        let inner = Obj::String(s_obj);
-        let ptr = Box::into_raw(Box::new(inner));
-        ObjPtr(ptr)
-    }
-
-    pub fn type_(&self) -> ObjType {
-        match self {
-            Obj::String(_) => ObjType::String,
-        }
-    }
-}
-
-pub struct StringObj {
-    len: usize,
-    chars: *const u8,
-}
-
-impl StringObj {
-    pub fn as_str(&self) -> &str {
-        // SAFETY: it is an invariant of this type to be a valid string slice
-        unsafe { std::str::from_utf8(slice::from_raw_parts(self.chars, self.len)).unwrap() }
-    }
-
-    pub fn concat(&self, other: &Self) -> ObjPtr {
-        let mut s = self.as_ref().to_owned();
-        s.push_str(other.as_str());
-        // We leak our string so that we don't clean it up when the function ends. This is okay
-        // because wsh has automaic memory management, and the returned pointer should be cleaned
-        // up later.
-        let leak = Box::leak(s.into_boxed_str());
-        Obj::string(leak)
-    }
-}
-
-impl AsRef<str> for StringObj {
-    fn as_ref(&self) -> &str {
-        self.as_str()
-    }
-}
-
-impl fmt::Display for StringObj {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.as_str())
-    }
-}
-
-impl PartialEq for StringObj {
-    fn eq(&self, other: &Self) -> bool {
-        self.as_str().eq(other.as_str())
-    }
-}
-
-impl Eq for StringObj {}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum ValueType {
     Number,
-    Null,
+    String,
     Boolean,
-    Object(ObjType),
+    Null,
 }
 
 impl fmt::Display for ValueType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             ValueType::Number => write!(f, "number"),
+            ValueType::String => write!(f, "string"),
+            ValueType::Boolean => write!(f, "boolean"),
             ValueType::Null => write!(f, "null"),
-            ValueType::Boolean => write!(f, "bool"),
-            ValueType::Object(ty) => write!(f, "object [{ty}]"),
         }
     }
 }
 
-impl fmt::Display for ObjType {
+impl fmt::Display for Value {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            ObjType::String => write!(f, "string"),
+            Value::Number(n) => write!(f, "{n}"),
+            Value::String(s) => write!(f, "{s}"),
+            Value::Boolean(b) => write!(f, "{b}"),
+            Value::Null => write!(f, "null"),
         }
     }
 }
